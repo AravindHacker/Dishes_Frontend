7b31b7ff82cbf1e872bf57add26e3cdaf4de15a1
AravindHacker <116102105+AravindHacker@users.noreply.github.com>
2024-07-21 11:32:13 +0530

    first commit

--- src/Components/index.js
+++ src/Components/index.js
@@ -1,10 +1,11 @@
-
 import React, { useEffect, useState } from 'react';
 import axios from 'axios';
 import { io } from 'socket.io-client';
-import './index.css'
+import './index.css';
 
-const socket = io('http://localhost:3001');
+const backendUrl = 'https://dishes-dashboard-bq90.onrender.com'; // Use your Render backend URL here
+
+const socket = io(backendUrl);
 
 const DishDashBoard = () => {
   const [dishes, setDishes] = useState([]);
@@ -21,7 +22,7 @@ const DishDashBoard = () => {
 
   const fetchDishes = async () => {
     try {
-      const response = await axios.get('http://localhost:3001/dishes');
+      const response = await axios.get(`${backendUrl}/dishes`);
       setDishes(response.data);
     } catch (error) {
       console.error('Error fetching dishes:', error);
@@ -30,7 +31,7 @@ const DishDashBoard = () => {
 
   const togglePublish = async (id) => {
     try {
-      await axios.post(`http://localhost:3001/toggle-publish/${id}`);
+      await axios.post(`${backendUrl}/toggle-publish/${id}`);
 
       // Optimistically update the state
       setDishes(prevDishes => 
@@ -49,9 +50,9 @@ const DishDashBoard = () => {
       <ul className='each-dish'>
         {dishes.map(dish => (
           <li key={dish.dishId} className='separate-dish-box'>
-            <h2 className='dish-name '>{dish.dishName}</h2>
+            <h2 className='dish-name'>{dish.dishName}</h2>
             <img src={dish.imageUrl} alt={dish.dishName} className='dish-image'/>
-           {/* <p className='dish-status'>Published: {dish.isPublished ? 'Yes' : 'No'}</p> */}
+            {/* <p className='dish-status'>Published: {dish.isPublished ? 'Yes' : 'No'}</p> */}
             <button onClick={() => togglePublish(dish.dishId)} className='toggel-btn'>
               {dish.isPublished ? 'Unpublish' : 'Publish'}
             </button>
